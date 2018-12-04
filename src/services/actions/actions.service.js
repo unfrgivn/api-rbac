dc4f89252e8672aeb0261de9ef04af2177f0b030
Brad <bradley.ash@gmail.com>
2018-12-04 00:18:18 -0500

    Fix tab formatting

--- src/services/actions/actions.service.js
+++ src/services/actions/actions.service.js
@@ -4,19 +4,19 @@ const createModel = require('../../models/actions.model');
 const hooks = require('./actions.hooks');
 
 module.exports = function (app) {
-  const Model = createModel(app);
-  const paginate = app.get('paginate');
+	const Model = createModel(app);
+	const paginate = app.get('paginate');
 
-  const options = {
-    Model,
-    paginate
-  };
+	const options = {
+		Model,
+		paginate
+	};
 
-  // Initialize our service with any options it requires
-  app.use('/actions', createService(options));
+	// Initialize our service with any options it requires
+	app.use('/actions', createService(options));
 
-  // Get our initialized service so that we can register hooks
-  const service = app.service('actions');
+	// Get our initialized service so that we can register hooks
+	const service = app.service('actions');
 
-  service.hooks(hooks);
+	service.hooks(hooks);
 };
